b25b2718607e890121b14f02b0943f6a7306bc13
MiquelGatius02 <113125375+MiquelGatius02@users.noreply.github.com>
2022-09-12 17:56:00 +0200

    Update main.java

--- src/main.java
+++ src/main.java
@@ -5,6 +5,7 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Tremenda prueba");
+		System.out.println("Estos cambios se han hecho desde otra rama");
 	}
 
 }
